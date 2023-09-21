9c3aeddf64349a4aee779c5d87677c9645c84d17
cb244 <cherica2132@gmail.com>
2023-09-21 10:02:20 +0900

    feat: 詳細画面の背景色をポケモンのタイプ色に変更

--- lib/poke_detail.dart
+++ lib/poke_detail.dart
@@ -12,78 +12,83 @@ class PokeDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<FavoritesNotifier>(
       builder: (context, favs, child) => Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ListTile(
-                  leading: IconButton(
-                    icon: const Icon(Icons.arrow_back),
-                    onPressed: () => Navigator.pop(context),
+        body: Container(
+          color: (pokeTypeColors[poke.types.first] ?? Colors.grey[100])
+              ?.withOpacity(.5),
+          child: SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ListTile(
+                    leading: IconButton(
+                      icon: const Icon(Icons.arrow_back),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    trailing: IconButton(
+                      icon: favs.isExist(poke.id)
+                          ? const Icon(Icons.star, color: Colors.orangeAccent)
+                          : const Icon(Icons.star_outline),
+                      onPressed: () => favs.toggle(poke.id),
+                    ),
                   ),
-                  trailing: IconButton(
-                    icon: favs.isExist(poke.id)
-                        ? const Icon(Icons.star, color: Colors.orangeAccent)
-                        : const Icon(Icons.star_outline),
-                    onPressed: () => favs.toggle(poke.id),
-                  ),
-                ),
-                const Spacer(),
-                Stack(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(32),
-                      child: Hero(
-                        tag: poke.name,
-                        child: Image.network(
-                          poke.imageUrl,
-                          height: 100,
-                          width: 100,
+                  const Spacer(),
+                  Stack(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(32),
+                        child: Hero(
+                          tag: poke.name,
+                          child: Image.network(
+                            poke.imageUrl,
+                            height: 100,
+                            width: 100,
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(8),
-                      child: Text(
-                        'No.${poke.id}',
-                        style: const TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        child: Text(
+                          'No.${poke.id}',
+                          style: const TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                Text(
-                  poke.name,
-                  style: const TextStyle(
-                      fontSize: 36, fontWeight: FontWeight.bold),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: poke.types
-                      .map((type) => Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 8),
-                            child: Chip(
-                              backgroundColor:
-                                  pokeTypeColors[type] ?? Colors.grey,
-                              label: Text(
-                                type,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: ThemeData.estimateBrightnessForColor(
-                                              pokeTypeColors[type] ??
-                                                  Colors.grey) ==
-                                          Brightness.light
-                                      ? Colors.black
-                                      : Colors.white,
+                    ],
+                  ),
+                  Text(
+                    poke.name,
+                    style: const TextStyle(
+                        fontSize: 36, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: poke.types
+                        .map((type) => Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8),
+                              child: Chip(
+                                backgroundColor:
+                                    pokeTypeColors[type] ?? Colors.grey,
+                                label: Text(
+                                  type,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: ThemeData.estimateBrightnessForColor(
+                                                pokeTypeColors[type] ??
+                                                    Colors.grey) ==
+                                            Brightness.light
+                                        ? Colors.black
+                                        : Colors.white,
+                                  ),
                                 ),
                               ),
-                            ),
-                          ))
-                      .toList(),
-                ),
-                const Spacer()
-              ],
+                            ))
+                        .toList(),
+                  ),
+                  const Spacer()
+                ],
+              ),
             ),
           ),
         ),
